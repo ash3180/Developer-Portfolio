f3592e11ae87e9c7e07f0650865460c7663ed848
Ashish Prajapati <prajapatiashish3180@gmail.com>
2026-06-24 19:22:25 +0530

    Setup scalable project architecture and design system

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import "./styles/variables.css";
+// import "./styles/global.css";
+// import "./styles/typography.css";
+// import "./styles/utilities.css";
+// import "./styles/animations.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+import "./styles/variables.css";
+import "./styles/global.css";
+import "./styles/typography.css";
+import "./styles/utilities.css";
+import "./styles/animations.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
-  </StrictMode>,
-)
+);
